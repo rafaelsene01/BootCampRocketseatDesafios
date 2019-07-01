1df68cc640d36b443414c1ab843bea9672284139
Rafael Sene Alves Penha <rafaelssene@gmail.com>
2019-07-01 17:14:51 -0300

    Configurando tratamento de erros com sentry

--- desafio-03/src/app.js
+++ desafio-03/src/app.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
+
 import routes from './routes';
+import sentryConfig from './config/sentry';
 
 import './database';
 
@@ -7,16 +12,28 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlawares();
     this.routes();
+    this.exectionHandler();
   }
 
   middlawares() {
+    this.server.use(Sentry.Handlers.errorHandler());
     this.server.use(express.json());
   }
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exectionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const error = await new Youch(err, req).toJSON();
+      return res.status(500).json(error);
+    });
   }
 }
 
---
+++ desafio-03/src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://3f82807052244716bd5e125e40117402@sentry.io/1494542',
+};
