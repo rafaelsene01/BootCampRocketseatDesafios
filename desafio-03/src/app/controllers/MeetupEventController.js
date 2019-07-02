51bb8e991904e1667fecb81bb7ada21287dde3db
Rafael Sene Alves Penha <rafaelssene@gmail.com>
2019-07-02 16:04:48 -0300

    Finalizado a parte de Listagem de meetups, passando data e pagina.

--- desafio-03/src/app/controllers/MeetupEventController.js
+++ desafio-03/src/app/controllers/MeetupEventController.js
@@ -1,16 +1,31 @@
 import { Op } from 'sequelize';
+import { isBefore, startOfDay, endOfDay, parseISO } from 'date-fns';
 import Meetup from '../models/Meetup';
 import User from '../models/User';
 import File from '../models/File';
 
 class MeetupEventController {
   async index(req, res) {
+    const page = req.query.page || 1;
+
+    const where = {
+      user_id: { [Op.ne]: req.userId },
+      data: { [Op.gt]: new Date() },
+    };
+
+    if (req.query.date) {
+      const searchDate = parseISO(req.query.date);
+
+      where.data = {
+        [Op.between]: [startOfDay(searchDate), endOfDay(searchDate)],
+      };
+    }
+
     const meetup = await Meetup.findAll({
-      where: {
-        user_id: { [Op.ne]: req.userId },
-        data: { [Op.gt]: new Date() },
-      },
-      order: [['data', 'DESC']],
+      where,
+      limit: 10,
+      offset: (page - 1) * 10,
+      order: [['data', 'ASC']],
       attributes: ['id', 'title', 'descricao', 'localizacao', 'data'],
       include: [
         {
