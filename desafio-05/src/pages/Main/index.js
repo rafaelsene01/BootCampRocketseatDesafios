756f00efbc15e7e064132ae2b2a6e1c95826c3eb
Rafael Sene Alves Penha <rafaelssene@gmail.com>
2019-07-04 12:36:45 -0300

    Iniciando o desafio-05, Captando erros ja concluido.

---
+++ desafio-05/src/pages/Main/index.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+
+import { FaGitAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+import api from '../../services/api';
+
+import { SubmitButton, Form, List, InputRepository } from './styles';
+import Container from '../../components/Container';
+
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+    inputError: false,
+  };
+
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
+
+  hendleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+
+    const { newRepo, repositories } = this.state;
+
+    try {
+      const response = await api.get(`/repos/${newRepo}`);
+
+      const data = {
+        name: response.data.full_name,
+      };
+
+      this.setState({
+        repositories: [...repositories, data],
+        newRepo: '',
+        inputError: false,
+      });
+    } catch {
+      this.setState({
+        inputError: true,
+      });
+    }
+
+    this.setState({
+      loading: false,
+    });
+  };
+
+  render() {
+    const { newRepo, loading, repositories, inputError } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGitAlt />
+          Repositories
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <InputRepository
+            type="text"
+            onChange={this.hendleInputChange}
+            value={newRepo}
+            placeholder="Adicionar repositorio"
+            inputError={inputError ? 1 : 0}
+          />
+
+          <SubmitButton loading={loading ? 1 : 0}>
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+
+        <List>
+          {repositories.map(repository => (
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
+            </li>
+          ))}
+        </List>
+      </Container>
+    );
+  }
+}
