96fac1f71ee0d8a4f3c62df15501d8541345a79d
Rafael Sene <rafaelssene@gmail.com>
2019-07-15 00:21:18 -0300

    init saga redux

--- desafio-07/src/index.js
+++ desafio-07/src/index.js
@@ -1,31 +1,18 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 import './config/ReactotronConfig';
+import { Provider } from 'react-redux';
 
 import Routes from './routes';
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return <Routes />;
-  }
+import store from './store';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  );
 }
 
-/* <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-      </View> */
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
+export default App;
