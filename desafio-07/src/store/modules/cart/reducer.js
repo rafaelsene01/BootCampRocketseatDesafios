a3d337398b111dc1d1fe592def4fd02dda518d7d
Rafael Sene <rafaelssene@gmail.com>
2019-07-15 03:55:33 -0300

    separating actions

--- desafio-07/src/store/modules/cart/reducer.js
+++ desafio-07/src/store/modules/cart/reducer.js
@@ -4,13 +4,8 @@ export default function cart(state = [], action) {
   switch (action.type) {
     case '@cart/ADD_SUCCESS':
       return produce(state, draft => {
-        const producIndex = draft.findIndex(p => p.id === action.product.id);
-
-        if (producIndex >= 0) {
-          draft[producIndex].amount += 1;
-        } else {
-          draft.push({ ...action.product, amount: 1 });
-        }
+        const { product } = action;
+        draft.push(product);
       });
     case '@cart/REMOVE':
       return produce(state, draft => {
