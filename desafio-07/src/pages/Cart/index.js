ea95ea26f901294421a5a967bbda5269b2f31f10
Rafael Sene <rafaelssene@gmail.com>
2019-07-15 04:47:45 -0300

    looking for stock

--- desafio-07/src/pages/Cart/index.js
+++ desafio-07/src/pages/Cart/index.js
@@ -29,13 +29,13 @@ import {
   RemoveButton,
 } from './styles';
 
-function Cart({ products, removeFromCart, updateAmount, total }) {
+function Cart({ products, removeFromCart, updateAmountRequest, total }) {
   function decrement(product) {
-    updateAmount(product.id, product.amount - 1);
+    updateAmountRequest(product.id, product.amount - 1);
   }
 
   function increment(product) {
-    updateAmount(product.id, product.amount + 1);
+    updateAmountRequest(product.id, product.amount + 1);
   }
   return (
     <Container>
--- desafio-07/src/store/modules/cart/reducer.js
+++ desafio-07/src/store/modules/cart/reducer.js
@@ -14,15 +14,11 @@ export default function cart(state = [], action) {
           draft.splice(producIndex, 1);
         }
       });
-    case '@cart/UPDATE_AMOUNT':
-      if (action.amount <= 0) {
-        return state;
-      }
-
+    case '@cart/UPDATE_AMOUNT_SUCCESS':
       return produce(state, draft => {
         const producIndex = draft.findIndex(p => p.id === action.id);
         if (producIndex >= 0) {
-          draft[producIndex].amount = action.amount;
+          draft[producIndex].amount = Number(action.amount);
         }
       });
     default:
