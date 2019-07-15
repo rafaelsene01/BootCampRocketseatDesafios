8971b68f89c0cf3506ff0ececb94a5e9602348b6
Rafael Sene <rafaelssene@gmail.com>
2019-07-15 00:39:40 -0300

    edit header

--- desafio-07/src/components/Header/index.js
+++ desafio-07/src/components/Header/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Container, Logo, LogoImg, BasketContainer, ItemCount } from './styles';
 
-export default function Header({ navigation }) {
+function Header({ navigation, cartSize }) {
   return (
     <Container>
       <Logo onPress={() => navigation.navigate('Main')}>
@@ -11,8 +12,12 @@ export default function Header({ navigation }) {
       </Logo>
       <BasketContainer onPress={() => navigation.navigate('Cart')}>
         <Icon name="shopping-basket" color="#FFF" size={30} />
-        <ItemCount>10</ItemCount>
+        <ItemCount>{cartSize}</ItemCount>
       </BasketContainer>
     </Container>
   );
 }
+
+export default connect(state => ({
+  cartSize: state.cart.length,
+}))(Header);
